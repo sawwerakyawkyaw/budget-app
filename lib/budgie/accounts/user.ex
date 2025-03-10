1d6f71c6f64ad998e6de54e5b2ccfcddb3d14863
Saw Wera Kyaw Kyaw <sk046@bucknell.edu>
2025-03-10 19:36:10 -0400

    Add name to user schema

--- lib/budgie/accounts/user.ex
+++ lib/budgie/accounts/user.ex
@@ -5,6 +5,7 @@ defmodule Budgie.Accounts.User do
   @foreign_key_type :binary_id
   schema "users" do
     field :email, :string
+    field :name, :string
     field :password, :string, virtual: true, redact: true
     field :hashed_password, :string, redact: true
     field :current_password, :string, virtual: true, redact: true
